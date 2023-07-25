dbdccc45ef8a7673221778ee5c14fbad71d0a42f
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-25 14:09:21 +0530

    [AkankshaAnil] Adding new contact to Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,7 +1,44 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBook
 {
-    private static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome To AddressBook Program");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fellow Learners!\n--Welcome to Address Book Problem--");
+
+            AddressBookManager addressBookManager = new AddressBookManager();
+
+            // Creating a new Contact and adding it to the Address Book
+            Contact newContact = new Contact("Snooki", "FF", "801 Zen Estate", "Pune", "MH", "345678", "234-678-2229", "snooki.ff@gmail.com");
+            addressBookManager.AddContact(newContact);
+
+            // Displaying all contacts in the address book
+            DisplayAddressBook(addressBookManager);
+
+            Console.WriteLine("Thank you for using the Address Book program!");
+        }
+
+        static void DisplayAddressBook(AddressBookManager addressBookManager)
+        {
+            var addressBook = addressBookManager.GetAddressBook();
+
+            Console.WriteLine("\nAddress Book Entries:");
+            foreach (Contact contact in addressBook)
+            {
+                Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
+                Console.WriteLine($"Address: {contact.Address}");
+                Console.WriteLine($"City: {contact.City}");
+                Console.WriteLine($"State: {contact.State}");
+                Console.WriteLine($"Zip Code: {contact.ZipCode}");
+                Console.WriteLine($"Phone: {contact.PhoneNumber}");
+                Console.WriteLine($"Email: {contact.Email}");
+                Console.WriteLine();
+            }
+        }
     }
 }
+
